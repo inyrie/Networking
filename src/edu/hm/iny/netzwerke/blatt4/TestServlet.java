751020f9e513a00169b011c5dd67c622b54b5352
Inyrie <inyrie@web.de>
2013-11-17 17:52:08 +0100

    blubb

---
+++ src/edu/hm/iny/netzwerke/blatt4/TestServlet.java
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+package edu.hm.iny.netzwerke.blatt4;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @author Stäff
+ */
+public class TestServlet extends HttpServlet {
+
+	/**
+	 * @param args
+	 */
+	public static void main(final String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
